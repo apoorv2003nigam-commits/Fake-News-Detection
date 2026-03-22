2a424fd694dc4545d58b5c899c0c1b8734f1d767
apoorv2003nigam-commits <apoorv2003nigam@gmail.com>
2026-03-23 01:06:55 +0530

    solving bug-1

--- app.py
+++ app.py
@@ -22,6 +22,5 @@ def predict():
     return render_template('index.html', prediction_text=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
     
-app.run(host='0.0.0.0', port=10000)
